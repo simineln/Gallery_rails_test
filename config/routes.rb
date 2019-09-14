4dcdc0beade75f513180cbe7134536b5eb293fe6
Nikita Siminel <siminel.n@gmail.com>
2019-09-14 11:51:54 +0300

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  get 'upload' => 'images#new', :as => 'upload'
+  get 'images/create'
+  get 'images/destroy'
+  get 'images/index'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
+  get 'categories/show'
+
+  resources :images, :categories
+
+  root 'images#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
